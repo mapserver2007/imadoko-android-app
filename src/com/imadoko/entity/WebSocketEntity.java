f34b3f950057a80bf91aa51b1f58b4ce282fd75c
mapserver2007 <mapserver2007@gmail.com>
2014-11-01 00:07:09 +0900

    Geofence用エンティティクラス追加、Javadoc追加

--- src/com/imadoko/entity/WebSocketEntity.java
+++ src/com/imadoko/entity/WebSocketEntity.java
@@ -1,23 +1,44 @@
 package com.imadoko.entity;
 
+/**
+ * WebSocket用エンティティクラス
+ * @author Ryuichi Tanaka
+ * @since 2014/10/31
+ */
 public class WebSocketEntity {
+    /** 認証キー */
     protected String _authKey;
-
+    /** リクエストID */
     protected String _requestId;
 
-
+    /**
+     * 認証キーを設定する
+     * @param authKey 認証キー
+     */
     public void setAuthKey(String authKey) {
         _authKey = authKey;
     }
 
+    /**
+     * 認証キーを返却する
+     * @return 認証キー
+     */
     public String getAuthKey() {
         return _authKey;
     }
 
+    /**
+     * リクエストIDを設定する
+     * @param requestId リクエストID
+     */
     public void setRequestId(String requestId) {
         _requestId = requestId;
     }
 
+    /**
+     * リクエストIDを返却する
+     * @return リクエストID
+     */
     public String getRequestId() {
         return _requestId;
     }
